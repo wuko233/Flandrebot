b5e66101c9360494b6b49b1c07481ea0f257ace7
wuko233 <77623212+wuko233@users.noreply.github.com>
2021-08-30 19:18:15 +0800

    修复与新增
    新增 戳一戳检测
    修复 茉莉对话@时无昵称
    优化 分类位置

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ def handle(msg):
         if msg["post_type"] == "meta_event":
             pass
         elif msg["post_type"] == "notice":
-            uid = msg["user_id"]
-            mid = msg["message_id"]
-            seq = msg["message_seq"]
+            ctrl.send("g", return_group, "这是一个非文本消息：\n" + str(msg))
+            if msg["sub_type"] == "poke" and msg["target_id"] == msg["self_id"]:
+                uid = msg["sender_id"]
+                if msg["group_id"] == None:
+                    ctrl.send("p", uid, ctrl.poke(uid) + "hi")
+                else:
+                    ctrl.send("g", msg["group_id"], ctrl.poke(uid))
         elif msg["post_type"] == "message":
                 if msg["message_type"] == "private":  #私聊消息
                     new_msg = msg["message"]
@@ -32,12 +36,14 @@ def handle(msg):
                     uid = msg["user_id"]
                     mid = msg["message_id"]
                     role = msg["sender"]["role"]
+                    nickname = msg["sender"]["nickname"]
+                    sex = msg["sender"]["sex"]
                     all_msg(uid, new_msg, gid)
-                    group_msg(uid, new_msg, gid)
+                    group_msg(uid, new_msg, gid, nickname, sex)
         else:
             ctrl.send("g", return_group, "无法判断的消息类型：\n" + msg)
     except KeyError:
-        print("有错误信息，已记录至“log.txt”")
+        print("有错误信息，已发送至调试群聊”")
         ctrl.send("g", return_group, "错误：\n" + str(msg))
 
 def read_write(route, content):
@@ -65,26 +71,9 @@ def group_msg(uid, msg, gid):
     """处理群消息"""
     if msg[0] == "#":
         other_msg = msg[:0:-1]
-        ml(other_msg, uid, gid)
+        api.ml(other_msg, uid, gid)
 
 def private_msg(uid, msg):
     """处理私聊消息"""
-    ml(msg, uid)
-
-"""QQ终端（测试）"""
-def sudo_send_msg(type, msg, id):
-    """主动发送消息"""
-    pass
-
-"""调用第三方接口"""
-def ml(msg, uid, gid = 0):
-    """调用茉莉机器人聊天接口"""
-    url = "http://i.itpk.cn/api.php?api_key=" + ml_api_key + "&api_secret=" + ml_api_secret + "&question=" + msg
-    r = requests.get(url)
-    new_msg = r.text
-    remsg = new_msg.replace("[user_name]", "你")
-    if gid == 0:
-        ctrl.send("p", uid, remsg)
-    else:
-        ctrl.send("g", gid, remsg)
+    api.ml(msg, uid， nickname)
 
